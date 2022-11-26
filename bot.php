c3c55fe5fcd3513746db35886040f02939e56a00
Mirosla4 <118390778+Mirosla4@users.noreply.github.com>
2022-11-26 21:11:04 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,39 +3,42 @@
     define("TG_TOKEN", "5866402087:AAEI38okRL_3MveD8a7eunhAFRPmdrdpg5w");
     define("TG_CHAT_ID", "1256885622");
 
+    $getQuery = [
+
+    ];
+    
+
+    //webhook
     // $getQuery = [
-    //     "chat_id" => TG_CHAT_ID,
-    //     "text" => "<b>Ass we can</b>",
-    //     "reply_markup" => json_encode(
-    //         [
-    //             'keyboard' => [
-    //                 [
-    //                     [
-    //                         "text" => "Gachi fun",
-    //                         "callback_data" => "test1",
-    //                     ],
-                        
-    //                 ]
-    //             ],
-    //             "one_time_keyboard" => TRUE,
-    //             "resize_keyboard" => TRUE,
-    //         ]
-    //     ),
+    //     "url" => "https://mirosla4.github.io/php_bots/bot.php",
     // ];
 
-    $getQuery = [
-        "url" => "https://hdom.org.ua/php/bot.php",
-    ];
+    function sendMessage($getQuery) {
+            $ch = curl_init("https://api.telegram.org/bot". TG_TOKEN ."/sendMessage?". http_build_query($getQuery) );
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+        
+            $resultQuery = curl_exec($ch);
+            curl_close($ch);
 
-    $ch = curl_init("https://api.telegram.org/bot". TG_TOKEN ."/sendWebhook?". http_build_query($getQuery) );
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
+            return $resultQuery;
+    }
 
-    $resultQuery = curl_exec($ch);
-    curl_close($ch);
-    
-    echo $resultQuery;
+    $data = file_get_contents("php://input");
+    $data = json_decode($data, 1);
+    var_dump($data);
+    $textMessage = $data["message"]["text"];
+
+    if($textMessage == "ХАЧУ ГАЧИ") {
+        $getQuery = [
+            "chat_id" => TG_CHAT_ID,
+            "text" => "<b>Ass we can<b>",
+            "parse_mode" => "html"
+        ];
+        sendMessage($getQuery);
+    }
+
 
 
     
